291c1b378b0e10b367604d9290c7f44d7e4662f8
Takao Suzuki <suzuki_taka@tdc.co.jp>
2020-12-23 23:40:40 +0900

    Update startContainers.sh

--- gitlab/startContainers.sh
+++ gitlab/startContainers.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "-------------------------"
-echo "Please input server ."
+echo "Please input server name."
 read input
 
 EXTERNAL_GITLAB_URL=$input /usr/local/bin/docker-compose up -d --remove-orphans
