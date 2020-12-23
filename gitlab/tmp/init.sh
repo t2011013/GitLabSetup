9760811098deafea3a6fd0ef7803eff32a6d581a
Takao Suzuki <suzuki_taka@tdc.co.jp>
2020-12-24 01:25:02 +0900

    Update init.sh

--- gitlab/tmp/init.sh
+++ gitlab/tmp/init.sh
@@ -1,5 +1,5 @@
 if [ ! -e '/etc/gitlab-runner/config.toml' ]; then
-  gitlab-runner register  --non-interactive  --executor "docker"  --docker-image alpine:latest --url $1 --registration-token "token" --description "docker-runner" --tag-list "docker,aws" --run-untagged="true" --locked="false" --access-level="not_protected"
+  gitlab-runner register  --non-interactive  --executor "docker"  --docker-image alpine:latest --url $1 --registration-token "token" --description "docker-runner" --tag-list "docker" --run-untagged="true" --locked="false" --access-level="not_protected"
   echo 'registration done.'
 else
   echo 'ok, runner is already registered.'
